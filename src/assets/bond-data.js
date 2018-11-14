f6c5ec8f00ef03c2df999df1d347dd840f67be5d
Erik <erikdmitchell@gmail.com>
2018-11-14 14:21:03 -0500

    added all film data

--- src/assets/bond-data.js
+++ src/assets/bond-data.js
@@ -34,7 +34,140 @@ window.BondData = {
             'year': 1967,
             'actor': 'Sean Connery',
             'director': 'Lewis Gilbert'
-        }
+        },
+        {
+            'id': 'bf6',
+            'title': 'On Her Majesty\'s Secret Service',
+            'year': 1969,
+            'actor': 'George Lazenby',
+            'director': 'Peter R. Hunt'
+        },        
+        {
+            'id': 'bf7',
+            'title': 'Diamonds Are Forever',
+            'year': 1971,
+            'actor': 'Sean Connery',
+            'director': 'Guy Hamilton'
+        },
+        {
+            'id': 'bf8',
+            'title': 'Live and Let Die',
+            'year': 1973,
+            'actor': 'Roger Moore',
+            'director': 'Guy Hamilton'
+        },
+        {
+            'id': 'bf9',
+            'title': 'The Man with the Golden Gun',
+            'year': 1974,
+            'actor': 'Roger Moore',
+            'director': 'Guy Hamilton'
+        },
+        {
+            'id': 'bf10',
+            'title': 'The Spy Who Loved Me',
+            'year': 1977,
+            'actor': 'Roger Moore',
+            'director': 'Lewis Gilbert'
+        },
+        {
+            'id': 'bf11',
+            'title': 'Moonraker',
+            'year': 1979,
+            'actor': 'Roger Moore',
+            'director': 'Lewis Gilbert'
+        },
+        {
+            'id': 'bf12',
+            'title': 'For Your Eyes Only',
+            'year': 1981,
+            'actor': 'Roger Moore',
+            'director': 'John Glen'
+        },                        
+        {
+            'id': 'bf13',
+            'title': 'Octopussy',
+            'year': 1983,
+            'actor': 'Roger Moore',
+            'director': 'John Glen'
+        }, 
+        {
+            'id': 'bf14',
+            'title': 'A View to a Kill',
+            'year': 1985,
+            'actor': 'Roger Moore',
+            'director': 'John Glen'
+        },                 
+        {
+            'id': 'bf15',
+            'title': 'The Living Daylights',
+            'year': 1987,
+            'actor': 'Timothy Dalton',
+            'director': 'John Glen'
+        }, 
+        {
+            'id': 'bf16',
+            'title': 'Licence to Kill',
+            'year': 1989,
+            'actor': 'Timothy Dalton',
+            'director': 'John Glen'
+        },         
+        {
+            'id': 'bf17',
+            'title': 'GoldenEye',
+            'year': 1995,
+            'actor': 'Pierce Brosnan',
+            'director': 'Martin Campbell'
+        },
+        {
+            'id': 'bf18',
+            'title': 'Tomorrow Never Dies',
+            'year': 1997,
+            'actor': 'Pierce Brosnan',
+            'director': 'Roger Spottiswoode'
+        },
+        {
+            'id': 'bf19',
+            'title': 'The World Is Not Enough',
+            'year': 1999,
+            'actor': 'Pierce Brosnan',
+            'director': 'Michael Apted'
+        },
+        {
+            'id': 'bf20',
+            'title': 'Die Another Day',
+            'year': 2002,
+            'actor': 'Pierce Brosnan',
+            'director': 'Lee Tamahori'
+        },                
+        {
+            'id': 'bf21',
+            'title': 'Casino Royale',
+            'year': 2006,
+            'actor': 'Daniel Craig',
+            'director': 'Martin Campbell'
+        }, 
+        {
+            'id': 'bf22',
+            'title': 'Quantum of Solace',
+            'year': 2008,
+            'actor': 'Daniel Craig',
+            'director': 'Marc Forster'
+        },
+        {
+            'id': 'bf23',
+            'title': 'Skyfall',
+            'year': 2012,
+            'actor': 'Daniel Craig',
+            'director': 'Sam Mendes'
+        },
+        {
+            'id': 'bf24',
+            'title': 'Spectre',
+            'year': 2015,
+            'actor': 'Daniel Craig',
+            'director': 'Sam Mendes'
+        }                
     ],
     'actors': [
         {
